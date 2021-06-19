124189ef798519cedb55d5671f20ea33c896078a
Youcef Aouali <you.aouali@gmail.com>
2021-06-19 15:01:53 +0200

    19/04 1ere fonction fetch et ebauche de carousel

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var best_img = document.getElementById("best_img");
 var best_title = document.getElementById("best_title");
 var best_desc = document.getElementById("best_desc");
 
-function fetch_top(url) {
-  fetch(url).then(resp => {
+function fetch_top(url_best) {
+  fetch(url_best).then(resp => {
     if(resp.ok){
       resp.json().then(data => {
         fetch(data.results[0].url)
@@ -14,11 +14,14 @@ function fetch_top(url) {
           best_title.innerHTML = data.title;
           best_desc.innerHTML = data.description
         })
-        .catch(error => {console.log("probleme de la reponse fetch" + error.message)});
+        .catch(error => {console.log("probleme reponse fetch" + error.message)});
       })
     } else {console.log("probleme reponse url")};
   })
   .catch(error => {console.log("probleme fetch" + error.message)});
 }
 
+
+
 fetch_top(url_best);
+
