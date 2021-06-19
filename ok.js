78d253c2df552a7d2666b8443aaee855f8f43de8
Youcef Aouali <you.aouali@gmail.com>
2021-06-19 14:59:05 +0200

    19/04 1ere fonction fetch, et ébauche de carousel.

--- ok.js
+++ ok.js
@@ -1,10 +1,7 @@
 var url_best = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
-var best_img = document.getElementById("best_img");
-var best_title = document.getElementById("best_title");
-var best_desc = document.getElementById("best_desc");
 
-function fetch_top(url) {
-  fetch(url).then(resp => {
+function fetch_top(url_best) {
+  fetch(url_best).then(resp => {
     if(resp.ok){
       resp.json().then(data => {
         fetch(data.results[0].url)
@@ -20,5 +17,48 @@ function fetch_top(url) {
   })
   .catch(error => {console.log("probleme fetch" + error.message)});
 }
+var best_img = document.getElementById("best_img");
+var best_title = document.getElementById("best_title");
+var best_desc = document.getElementById("best_desc");
+
+var url_comedy = "http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score";
+var url_horror = "http://localhost:8000/api/v1/titles/?genre=Horror&sort_by=-imdb_score";
+var url_action = "http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score";
+
+
+const sliders = document.querySelector(".carouselbox");
+var scrollPerClick;
+var ImagePadding = 7;
+
+// Scroll Functionality
+var scrollAmount = 0;
+
+function sliderScrollLeft() {
+  sliders.scrollTo({
+    top: 0,
+    left: (scrollAmount -= scrollPerClick),
+    behavior: "smooth",
+  });
+
+  if (scrollAmount < 0) {
+    scrollAmount = 0;
+  }
+
+  console.log("Scroll Amount: ", scrollAmount);
+}
+
+function sliderScrollRight() {
+  if (scrollAmount <= sliders.scrollWidth - sliders.clientWidth) {
+    sliders.scrollTo({
+      top: 0,
+      left: (scrollAmount += scrollPerClick),
+      behavior: "smooth",
+    });
+  }
+  console.log("Scroll Amount: ", scrollAmount);
+}
+scrollPerClick = document.querySelector(".img-1").clientWidth + 20;
+
 
 fetch_top(url_best);
+
