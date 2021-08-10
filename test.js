f6bcb55fecdcaddd0d23c7433ec9d0ff73aed87f
Youcef Aouali <you.aouali@gmail.com>
2021-08-10 15:03:49 +0200

    10/08 manque le fetch de la modale

--- test.js
+++ test.js
@@ -4,80 +4,113 @@ var best_title = document.getElementById("best_title");
 var best_desc = document.getElementById("best_desc");
 
 async function fetch_best(url_best) {
-  fetch(url_best).then(resp => {
-    if(resp.ok){
-      resp.json().then(data => {
-        fetch(data.results[0].url)
-        .then((resp) => resp.json())
-        .then(data => {
-          best_img.src = data.image_url;
-          best_title.innerHTML = data.title;
-          best_desc.innerHTML = data.description
-        })
-        .catch(error => {console.log("probleme reponse fetch" + error.message)});
-      })
-    } else {console.log("probleme reponse url")};
-  })
-  .catch(error => {console.log("probleme fetch" + error.message)});
+  fetch(url_best)
+    .then((resp) => {
+      if (resp.ok) {
+        resp.json().then((data) => {
+          fetch(data.results[0].url)
+            .then((resp) => resp.json())
+            .then((data) => {
+              best_img.src = data.image_url;
+              best_title.innerHTML = data.title;
+              best_desc.innerHTML = data.description;
+            })
+            .catch((error) => {
+              console.log("probleme reponse fetch" + error.message);
+            });
+        });
+      } else {
+        console.log("probleme reponse url");
+      }
+    })
+    .catch((error) => {
+      console.log("probleme fetch" + error.message);
+    });
 }
 
 const url_best = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
-var liste_best = [
-  document.getElementById("best1"), document.getElementById("best2"),
-  document.getElementById("best3"), document.getElementById("best4"),
-  document.getElementById("best5"), document.getElementById("best6"),
-  document.getElementById("best7")
+const liste_best = [
+  document.getElementById("best1"),
+  document.getElementById("best2"),
+  document.getElementById("best3"),
+  document.getElementById("best4"),
+  document.getElementById("best5"),
+  document.getElementById("best6"),
+  document.getElementById("best7"),
 ];
 
-
-const url_com = "http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score"
-var liste_cat1 = [
-  document.getElementById("com1"), document.getElementById("com2"),
-  document.getElementById("com3"), document.getElementById("com4"),
-  document.getElementById("com5"), document.getElementById("com6"),
-  document.getElementById("com7")
+const url_com =
+  "http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score";
+const liste_cat1 = [
+  document.getElementById("com1"),
+  document.getElementById("com2"),
+  document.getElementById("com3"),
+  document.getElementById("com4"),
+  document.getElementById("com5"),
+  document.getElementById("com6"),
+  document.getElementById("com7"),
 ];
 
-const url_hor = "http://localhost:8000/api/v1/titles/?genre=Horror&sort_by=-imdb_score";
-var liste_cat2 = [
-  document.getElementById("horror1"), document.getElementById("horror2"),
-  document.getElementById("horror3"), document.getElementById("horror4"),
-  document.getElementById("horror5"), document.getElementById("horror6"),
-  document.getElementById("horror7")
+const url_hor =
+  "http://localhost:8000/api/v1/titles/?genre=Horror&sort_by=-imdb_score";
+const liste_cat2 = [
+  document.getElementById("horror1"),
+  document.getElementById("horror2"),
+  document.getElementById("horror3"),
+  document.getElementById("horror4"),
+  document.getElementById("horror5"),
+  document.getElementById("horror6"),
+  document.getElementById("horror7"),
 ];
 
-const url_act = "http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score";
+const url_act =
+  "http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score";
 var liste_cat3 = [
-  document.getElementById("action1"), document.getElementById("action2"),
-  document.getElementById("action3"), document.getElementById("action4"),
-  document.getElementById("action5"), document.getElementById("action6"),
-  document.getElementById("action7")
+  document.getElementById("action1"),
+  document.getElementById("action2"),
+  document.getElementById("action3"),
+  document.getElementById("action4"),
+  document.getElementById("action5"),
+  document.getElementById("action6"),
+  document.getElementById("action7"),
 ];
 
 function fetch_cat(url, liste_best) {
-  fetch(url).then(resp => {
-    if(resp.ok){
-      resp.json().then(data => {
-      for (let nb = 0; nb < 5; nb++) {
-        liste_best[nb].src = data.results[nb].image_url;
-        liste_best[nb].innerHTML = data.results[nb].url}
-      fetch(data.next).then(resp => {
-        if(resp.ok){
-          resp.json().then(data => {
-          for (let nb = 0; nb < 2; nb++) {
-            liste_best[nb+5].src = data.results[nb].image_url;
-            liste_best[nb+5].innerHTML = data.results[nb].url}
-          // then
-         // liste catégorie liste_comédie[nb+5].src = data.results[nb].image_url;
-         //etc ????????
-          })
-        } else {console.log("probleme reponse url")};
-      })
-      .catch(function(error) {console.log("probleme reponse fetch" + error.message)});
+  fetch(url)
+    .then((resp) => {
+      if (resp.ok) {
+        resp.json().then((data) => {
+          for (let nb = 0; nb < 5; nb++) {
+            liste_best[nb].src = data.results[nb].image_url;
+            liste_best[nb].innerHTML = data.results[nb].url;
+          }
+          fetch(data.next)
+            .then((resp) => {
+              if (resp.ok) {
+                resp.json().then((data) => {
+                  for (let nb = 0; nb < 2; nb++) {
+                    liste_best[nb + 5].src = data.results[nb].image_url;
+                    liste_best[nb + 5].innerHTML = data.results[nb].url;
+                  }
+                  // then
+                  // liste catégorie liste_comédie[nb+5].src = data.results[nb].image_url;
+                  //etc ????????
+                });
+              } else {
+                console.log("probleme reponse url");
+              }
+            })
+            .catch(function (error) {
+              console.log("probleme reponse fetch" + error.message);
+            });
+        });
+      } else {
+        console.log("probleme reponse url");
+      }
     })
-  } else {console.log("probleme reponse url")};
-  })
-  .catch(function(error) {console.log("probleme reponse fetch" + error.message)});
+    .catch(function (error) {
+      console.log("probleme reponse fetch" + error.message);
+    });
 }
 
 fetch_best(url_best);
@@ -85,6 +118,5 @@ fetch_cat(url_best, liste_best);
 fetch_cat(url_com, liste_cat1);
 fetch_cat(url_hor, liste_cat2);
 fetch_cat(url_act, liste_cat3);
-
 
 
--- testmodal.js
+++ testmodal.js
@@ -1,25 +1,59 @@
-// Get the modal
-var modal = document.getElementsByClassName("modal");
+/* ---- déclaration des elements du DOM ---- */
 
-// Get the button that opens the modal
-var btn = document.querySelectorAll("carousel__item");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-btn");
-
-// When the user clicks on the button, open the modal
-var elems = document.getElementsByClassName('carousel__item');
-for (var i=0;i<elems.length;i+=1){
-  elems[i].style.display = 'block';
+var modal = document.querySelector(".modal");
+var closebtn = document.querySelector(".close-btn");
+var element = document.getElementsByClassName(".carousel__item");
+for(var i = 0; i < element.length; i = i + 1) {
+  modal.style.display="block";
 }
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+  
+
+
+var id_image_url  = document.getElementById("id_image_url");
+var id_title  = document.getElementById("id_title");
+var id_genres  = document.getElementById("id_genres");
+var best_modal = document.getElementById("best_modal");
+
+var btn_best = [document.getElementById("btn_best1"), document.getElementById("btn_best2"), 
+                document.getElementById("btn_best3"),document.getElementById("btn_best4"), 
+                document.getElementById("btn_best5"), document.getElementById("btn_best6"),
+                document.getElementById("btn_best7")
+];
+
+
+best_modal.onclick = function() {print_modal_info(best1)};
+for (let nb in btn_best) {
+  btn_best[nb].onclick = function() {
+    if (liste_best[nb]) {
+      print_modal_info(liste_best[nb])
+
+}}}
+
+// fonction pour fermer la fenêtre avec le  boutton
+closebtn.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// fonction pour fermer la fenêtre en cliquanr n'importe ou sur la fenêtre
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+
+function print_modal_info(url_best) {
+  modal.style.display = "block";
+  fetch(url_best).then(resp => {
+    if(resp.ok){
+      resp.json().then(data => {
+        id_image_url.src = data.image_url;
+        id_title.innerHTML = data.original_title;
+        id_genres.innerHTML = data.genres;
+      })
+    } else {console.log("probleme de reponse")};
+  })
+  .catch(function(error) {console.log("probleme lors de l'operation fetch" + error.message)});
+}
+
+print_modal_info(url_best);
